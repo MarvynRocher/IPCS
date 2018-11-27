ddba995f669831df155fce366bcaf71bd47840a0
MarvynRocher <38289869+MarvynRocher@users.noreply.github.com>
2018-11-27 09:43:26 +0100

    Add files via upload

---
+++ 00_Sockets_C/02_ServeurWeb_Tchat/ServeurHTTP/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* le serveur HTTP est en mode connecté donc on utilise le protocole TCP */
+int main(int argc, char** argv) {
+
+
+    return (EXIT_SUCCESS);
+}
+
